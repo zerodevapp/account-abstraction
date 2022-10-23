d95f33480975467cf7c1fba1af3c1ac0b45f3448
Derek Chiang <me@derekchiang.com>
2022-10-23 19:22:11 -0400

    Make it so that SimpleWalletDeployer does not revert when the wallet already exists, so it's possible for clients to query the address

--- contracts/samples/SimpleWalletDeployer.sol
+++ contracts/samples/SimpleWalletDeployer.sol
@@ -8,8 +8,26 @@ import "./SimpleWallet.sol";
  * the "initCode" for a wallet hold its address and a method call (deployWallet) with parameters, not actual constructor code.
  */
 contract SimpleWalletDeployer {
+    function isDeployed(address _addr) private view returns (bool isContract){
+        uint32 size;
+        assembly {
+            size := extcodesize(_addr)
+        }
+        return (size > 0);
+    }
+
+    function getWalletAddress(IEntryPoint entryPoint, address owner, uint salt) public view returns (address) {
+        bytes32 bytecodeHash = keccak256(abi.encodePacked(type(SimpleWallet).creationCode, abi.encode(entryPoint, owner)));
+        bytes32 _data = keccak256(abi.encodePacked(bytes1(0xff), address(this), bytes32(salt), bytecodeHash));
+        return address(uint160(uint256(_data)));
+    }
 
     function deployWallet(IEntryPoint entryPoint, address owner, uint salt) public returns (SimpleWallet) {
-        return new SimpleWallet{salt : bytes32(salt)}(entryPoint, owner);
+        address payable walletAddr = payable(getWalletAddress(entryPoint, owner, salt));
+        if (isDeployed(walletAddr)) {
+            return SimpleWallet(walletAddr);
+        } else {
+            return new SimpleWallet{salt : bytes32(salt)}(entryPoint, owner);
+        }
     }
 }
