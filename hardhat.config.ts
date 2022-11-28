14f86cee8c45d4e6f07ba1326cee688827b16f23
Derek Chiang <me@derekchiang.com>
2022-11-28 13:53:23 -0500

    Deploy on Avalanche mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,6 +73,10 @@ const config: HardhatUserConfig = {
       url: `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_ID}`,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY!, process.env.MUMBAI_PAYMASTER_OWNER_PRIVATE_KEY!],
     },
+    avalanche: {
+      url: `https://avalanche-mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY!, process.env.MUMBAI_PAYMASTER_OWNER_PRIVATE_KEY!],
+    },
   },
   namedAccounts: {
     paymasterOwner: {
