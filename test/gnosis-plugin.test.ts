58985ae553b9ba39c7d39a638ecd57ab940bf983
leekt <leekt216@gmail.com>
2023-02-23 02:31:52 +0900

    test for multiple usage of single session

--- test/gnosis-plugin.test.ts
+++ test/gnosis-plugin.test.ts
@@ -119,101 +119,8 @@ describe.only('Zerodev Plugin - Gnosis Proxy', function () {
 
 
   describe('session key', async function(){
-    async function getSessionNonce(
-      plugin : EIP4337SessionKeyPlugin,
-      sender : string,
-      sessionKey : Signer
-    ) : Promise<number> {
-      return await EIP4337PluginFallback__factory.connect(sender, plugin.provider).callStatic
-      .queryPlugin(plugin.address, plugin.interface.encodeFunctionData('sessionNonce', [await sessionKey.getAddress()])).catch(e => {
-        if(e.errorName !== "QueryResult") {
-          throw e;
-        }
-        return e.errorArgs.result;
-      });
-    }
-    
-    async function fillAndSignSessionKey(
-      owner : Signer,
-      userOp : Partial<UserOperation>,
-      validUntil: number,
-      validAfter:number,
-      plugin : EIP4337SessionKeyPlugin,
-      sessionKey : Signer,
-      entryPoint?: EntryPoint
-    ) : Promise<UserOperation> {
-      const provider = entryPoint?.provider
-      let op = await fillUserOp(userOp, entryPoint)
-    
-      const domain = {
-        name : "EIP4337PluginFallback",
-        version: "1.0.0",
-        verifyingContract: fallback.address,
-        chainId: (await provider!.getNetwork()).chainId
-      }
-      const value = {
-        sender : userOp.sender,
-        nonce : (await (await GnosisSafe__factory.connect(userOp.sender!, owner)).nonce()).toString(),
-        validUntil : validUntil,
-        validAfter : validAfter,
-        plugin : plugin.address,
-        data : hexZeroPad(await sessionKey.getAddress(), 20)
-      }
-      const userSig = await owner._signTypedData(
-          domain,
-          {
-            "ValidateUserOpPlugin" : [
-              {name: "sender", type: "address"},
-              {name: "nonce", type: "uint256"},
-              {name: "validUntil", type: "uint48"},
-              {name: "validAfter", type: "uint48"},
-              {name: "plugin", type: "address"},
-              {name: "data", type: "bytes"}
-            ]
-          },
-          value
-      );
-
-      const chainId = await provider!.getNetwork().then(net => net.chainId)
-      const opHash = await getUserOpHash(op, entryPoint!.address, chainId);
-      
-      const sessionDomain = {
-        name : "EIP4337SessionKeyPlugin",
-        version: "1.0.0",
-        verifyingContract: userOp.sender,
-        chainId: chainId
-      };
-
-      const nonce = await getSessionNonce(plugin, userOp.sender!, sessionKey);
-      console.log("nonce : ", nonce);
-      const sessionKeySig = await sessionKey._signTypedData(
-        sessionDomain,
-        {
-          "Session" : [
-            {name: "userOpHash", type: "bytes32"},
-            {name: "nonce", type: "uint256"},
-          ]
-        },
-        {
-          userOpHash : opHash,
-          nonce : nonce //await plugin.sessionNonce(await sessionKey.getAddress())
-        }
-      );
-
-      const signature = hexConcat([
-        hexZeroPad(plugin.address, 20),
-        hexZeroPad(validUntil, 6),
-        hexZeroPad(validAfter, 6),
-        userSig,
-        ethers.utils.defaultAbiCoder.encode(["bytes","bytes"], [await sessionKey.getAddress(), sessionKeySig])
-      ]);
-      return {
-        ...op,
-        signature
-      };
-    }
     let sessionKey : Signer;
-    let signedUserOp : UserOperation;
+    let userSignedUserOp : UserOperation;
     beforeEach(async function() {
         sessionKey = createAccountOwner(); // use random session key
         let userOp = {
@@ -222,8 +129,9 @@ describe.only('Zerodev Plugin - Gnosis Proxy', function () {
           callData: safe_execTxCallData,
           verificationGasLimit: 1e6,
         }
-        signedUserOp = await fillAndSignSessionKey(
+        userSignedUserOp = await signSessionKey(
           owner,
+          fallback,
           userOp,
           1777068462,
           0,
@@ -239,6 +147,13 @@ describe.only('Zerodev Plugin - Gnosis Proxy', function () {
     });
 
     it("should exec", async function() {
+      const signedUserOp = await signUserOpWithSessionKey(
+        userSignedUserOp,
+        sessionKey,
+        sessionKeyPlugin,
+        entryPoint
+      );
+
       const count_before = await counter.counters(proxy.address);
       const nonce_before = await getSessionNonce(sessionKeyPlugin, proxy.address, sessionKey);
       const rcpt = await entryPoint.handleOps([signedUserOp], beneficiary).then(async r => r.wait())
@@ -247,7 +162,148 @@ describe.only('Zerodev Plugin - Gnosis Proxy', function () {
       expect(await counter.counters(proxy.address)).to.eq(count_before.add(1));
       const nonce_after = await getSessionNonce(sessionKeyPlugin, proxy.address, sessionKey);
       expect(nonce_after).to.be.eq(ethers.BigNumber.from(nonce_before).add(1));
+    });
+    it("should be able to exec multiple times with same user signature", async function() {
+      let signedUserOp = await signUserOpWithSessionKey(
+        userSignedUserOp,
+        sessionKey,
+        sessionKeyPlugin,
+        entryPoint
+      );
 
+      let count_before = await counter.counters(proxy.address);
+      let nonce_before = await getSessionNonce(sessionKeyPlugin, proxy.address, sessionKey);
+      let rcpt = await entryPoint.handleOps([signedUserOp], beneficiary).then(async r => r.wait())
+      let ev = rcpt.events!.find(ev => ev.event === 'UserOperationEvent')!
+      expect(ev.args!.success).to.eq(true)
+      expect(await counter.counters(proxy.address)).to.eq(count_before.add(1));
+      let nonce_after = await getSessionNonce(sessionKeyPlugin, proxy.address, sessionKey);
+      expect(nonce_after).to.be.eq(ethers.BigNumber.from(nonce_before).add(1));
+
+      signedUserOp = await signUserOpWithSessionKey(
+        userSignedUserOp,
+        sessionKey,
+        sessionKeyPlugin,
+        entryPoint
+      );
+
+      count_before = await counter.counters(proxy.address);
+      nonce_before = await getSessionNonce(sessionKeyPlugin, proxy.address, sessionKey);
+      rcpt = await entryPoint.handleOps([signedUserOp], beneficiary).then(async r => r.wait())
+      ev = rcpt.events!.find(ev => ev.event === 'UserOperationEvent')!
+      expect(ev.args!.success).to.eq(true)
+      expect(await counter.counters(proxy.address)).to.eq(count_before.add(1));
+      nonce_after = await getSessionNonce(sessionKeyPlugin, proxy.address, sessionKey);
+      expect(nonce_after).to.be.eq(ethers.BigNumber.from(nonce_before).add(1));
     });
   })
 })
+
+async function getSessionNonce(
+  plugin : EIP4337SessionKeyPlugin,
+  sender : string,
+  sessionKey : Signer
+) : Promise<number> {
+  return await EIP4337PluginFallback__factory.connect(sender, plugin.provider).callStatic
+  .queryPlugin(plugin.address, plugin.interface.encodeFunctionData('sessionNonce', [await sessionKey.getAddress()])).catch(e => {
+    if(e.errorName !== "QueryResult") {
+      throw e;
+    }
+    return e.errorArgs.result;
+  });
+}
+
+async function signSessionKey(
+  owner : Signer,
+  fallback : EIP4337PluginFallback,
+  userOp : Partial<UserOperation>,
+  validUntil: number,
+  validAfter:number,
+  plugin : EIP4337SessionKeyPlugin,
+  sessionKey : Signer,
+  entryPoint?: EntryPoint
+) : Promise<UserOperation> {
+  const op = await fillAndSign(userOp, owner, entryPoint)
+  const provider = entryPoint?.provider
+  const domain = {
+    name : "EIP4337PluginFallback",
+    version: "1.0.0",
+    verifyingContract: fallback.address,
+    chainId: (await provider!.getNetwork()).chainId
+  }
+  const value = {
+    sender : userOp.sender,
+    nonce : (await (await GnosisSafe__factory.connect(userOp.sender!, owner)).nonce()).toString(),
+    validUntil : validUntil,
+    validAfter : validAfter,
+    plugin : plugin.address,
+    data : hexZeroPad(await sessionKey.getAddress(), 20)
+  }
+  const userSig = await owner._signTypedData(
+      domain,
+      {
+        "ValidateUserOpPlugin" : [
+          {name: "sender", type: "address"},
+          {name: "nonce", type: "uint256"},
+          {name: "validUntil", type: "uint48"},
+          {name: "validAfter", type: "uint48"},
+          {name: "plugin", type: "address"},
+          {name: "data", type: "bytes"}
+        ]
+      },
+      value
+  );
+
+  const signature = hexConcat([
+    hexZeroPad(plugin.address, 20),
+    hexZeroPad(validUntil, 6),
+    hexZeroPad(validAfter, 6),
+    userSig,
+  ]);
+  return {
+    ...op,
+    nonce : value.nonce,
+    signature : signature
+  };
+}
+
+async function signUserOpWithSessionKey(
+  userOp : UserOperation,
+  sessionKey : Signer,
+  plugin : EIP4337SessionKeyPlugin,
+  entryPoint?: EntryPoint
+): Promise<UserOperation> {
+  let op = await fillUserOp(userOp, entryPoint);
+  const provider = entryPoint?.provider;
+  const chainId = await provider!.getNetwork().then(net => net.chainId);
+  const opHash = await getUserOpHash(op, entryPoint!.address, chainId);
+  const sessionDomain = {
+    name : "EIP4337SessionKeyPlugin",
+    version: "1.0.0",
+    verifyingContract: userOp.sender,
+    chainId: chainId
+  };
+
+  const nonce = await getSessionNonce(plugin, userOp.sender!, sessionKey);
+  const sessionKeySig = await sessionKey._signTypedData(
+    sessionDomain,
+    {
+      "Session" : [
+        {name: "userOpHash", type: "bytes32"},
+        {name: "nonce", type: "uint256"},
+      ]
+    },
+    {
+      userOpHash : opHash,
+      nonce : nonce //await plugin.sessionNonce(await sessionKey.getAddress())
+    }
+  );
+
+  return {
+    ...op,
+    signature : hexConcat([
+      op.signature,
+      ethers.utils.defaultAbiCoder.encode(["bytes","bytes"], [await sessionKey.getAddress(), sessionKeySig])
+    ])
+  };
+}
