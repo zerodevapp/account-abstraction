8fa942792f3bca4ccc718593ad1739256e780e92
leekt <leekt216@gmail.com>
2023-03-01 22:44:29 +0900

    trying ethers

--- deploy/0_deploy_entrypoint.ts
+++ deploy/0_deploy_entrypoint.ts
@@ -1,10 +1,11 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { Create2Factory } from '../src/Create2Factory'
+import { ethers } from 'hardhat';
 
 const deployEntryPoint: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const from = await hre.ethers.provider.getSigner().getAddress()
-  await new Create2Factory(hre.ethers.provider).deployFactory()
+  const from = await ethers.provider.getSigner().getAddress()
+  await new Create2Factory(ethers.provider).deployFactory()
 
   console.log("from : ", from);
 
